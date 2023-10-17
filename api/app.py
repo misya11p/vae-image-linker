be3a4e9f84fb7d3745e9bd00ea9e793e788ec8ea
misya11p <miya33ps@gmail.com>
2023-10-18 03:41:14 +0900

    config

--- api/app.py
+++ api/app.py
@@ -9,9 +9,15 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
+CONFIG = {
+    "model_path": "models/model.pth",
+    "n_frames": 10,
+    "device": "cpu",
+}
+
 @app.route("/image-linker", methods=["POST"])
 def image_linker():
     image1 = request.files["image1"]
     image2 = request.files["image2"]
-    result = link_images(image1, image2)
+    result = link_images(image1, image2, CONFIG)
     return result
