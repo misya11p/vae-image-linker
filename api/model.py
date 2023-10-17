ae9981e95ff4e3f6a561c010299815762864c179
misya11p <miya33ps@gmail.com>
2023-10-17 21:06:47 +0900

    model

---
+++ api/model.py
@@ -0,0 +1,49 @@
+import torch
+from torch import nn
+
+
+class VAE(nn.Module):
+    def __init__(self, data_size: int, device: str):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Flatten(),
+            self._linear_block(data_size, 512),
+            self._linear_block(512, 128),
+            nn.Linear(128, 64*2),
+        )
+        self.encoder = nn.Sequential(
+            self._linear_block(64, 128),
+            self._linear_block(128, 512),
+            nn.Linear(512, data_size),
+            nn.Sigmoid()
+        )
+        self.device = device
+        self.to(device)
+
+    @staticmethod
+    def _linear_block(input_size, output_size):
+        net = nn.Sequential(
+            nn.Linear(input_size, output_size),
+            nn.BatchNorm1d(output_size),
+            nn.ReLU()
+        )
+        return net
+
+    def forward(self, x):
+        z, mean, log_var = self.encode(x)
+        y = self.decoder(z).view(-1, 1, 28, 28)
+        return y, mean, log_var, z
+
+    def representation(self, mean, log_var):
+        norm = torch.randn_like(mean)
+        z = mean + torch.exp(log_var/2)*norm
+        return z
+
+    def encode(self, x):
+        mean, log_var = self.encoder(x).chunk(2, dim=1)
+        z = self.representation(mean, log_var).to(self.device)
+        return z, mean, log_var
+
+    def decode(self, z):
+        y = self.decoder(z)
+        return y
