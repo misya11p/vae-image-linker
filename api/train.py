4b7777d15e721f40cd6c3c226ee71e8674c8373c
misya11p <miya33ps@gmail.com>
2023-10-18 02:44:59 +0900

    train

--- api/train.py
+++ api/train.py
@@ -3,19 +3,18 @@ import torch
 from torch import optim
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
-from torchvision.datasets import STL10
-from torchvision import transforms
 from dlprog import train_progress
-from .model import VAE
+from model import VAE
+from train_data import get_dataloader
 
 prog = train_progress()
-DATA_SIZE = 3*96*96
 
 
 def main(
     device: str = "auto",
+    data_root: str = "./data",
+    image_size: int = 128,
     save_path: str = "model.pth",
-    data_path: str = "datasets",
     n_epochs: int = 30,
     lr: float = 1e-4,
     batch_size: int = 64,
@@ -23,27 +22,13 @@ def main(
     if device == "auto":
         device = "cuda" if torch.cuda.is_available() else "cpu"
     device = torch.device(device)
-    dataloader = get_dataloader(data_path, batch_size)
-    model = VAE(DATA_SIZE, device)
+    dataloader = get_dataloader(data_root, image_size, batch_size)
+    model = VAE(image_size, device)
     optimizer = optim.Adam(model.parameters(), lr=lr)
     print("start training")
     train(model, optimizer, dataloader, device, n_epochs)
     model.save(save_path)
     print("finish training")
-
-
-def get_dataloader(
-    data_path: str = "datasets",
-    batch_size: int = 64
-) -> DataLoader:
-    dataset = STL10(
-        root=data_path,
-        split="train",
-        download=True,
-        transform=transforms.ToTensor()
-    )
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-    return dataloader
 
 
 def loss_fn(x, y, mean, log_var):
@@ -61,7 +46,7 @@ def train(
     model.train()
     prog.start(n_epochs=n_epochs, n_iter=len(dataloader))
     for _ in range(n_epochs):
-        for x, _ in dataloader:
+        for x in dataloader:
             optimizer.zero_grad()
             x = x.to(device)
             y, mean, log_var, _ = model(x)
@@ -74,8 +59,9 @@ def train(
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--device", type=str, default="auto")
+    parser.add_argument("--data_root", type=str, default="./data")
+    parser.add_argument("--image_size", type=int, default=128)
     parser.add_argument("--save_path", type=str, default="model.pth")
-    parser.add_argument("--data_path", type=str, default="./datasets")
     parser.add_argument("--n_epochs", type=int, default=30)
     parser.add_argument("--lr", type=float, default=1e-4)
     parser.add_argument("--batch_size", type=int, default=64)
--- api/train_data.py
+++ api/train_data.py
@@ -1,3 +1,4 @@
+import os
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision.datasets import STL10, ImageFolder
@@ -8,6 +9,7 @@ class ImageDataset(Dataset):
     def __init__(self, image_dataset, line_image_dataset, image_size=96):
         transform = transforms.Compose([
             transforms.Resize(image_size),
+            transforms.Grayscale(),
             transforms.ToTensor()
         ])
         image_data = torch.stack(
@@ -27,12 +29,12 @@ class ImageDataset(Dataset):
 
 
 def get_dataloader(
-    image_path: str = "./data",
-    line_image_path: str = "./data/line-images-folder",
-    image_size: int = 96,
+    data_root: str = "./data",
+    image_size: int = 128,
     batch_size: int = 64
 ) -> DataLoader:
-    image_dataset = STL10(root=image_path, split="train")
+    line_image_path = os.path.join(data_root, "line-images-folder")
+    image_dataset = STL10(root=data_root, split="test")
     line_image_dataset = ImageFolder(root=line_image_path)
     dataset = ImageDataset(image_dataset, line_image_dataset, image_size)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
